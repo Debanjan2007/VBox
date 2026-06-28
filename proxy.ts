ffe22ecf575851488196b9d6a2a0476ca78b7796
Debanjan Das <microsoft2420@gmail.com>
2026-06-28 20:48:11 +0530

    protect all routes that comes after /dashboard

--- proxy.ts
+++ proxy.ts
@@ -1,6 +1,12 @@
-﻿import { clerkMiddleware } from '@clerk/nextjs/server'
+﻿import { clerkMiddleware , createRouteMatcher } from '@clerk/nextjs/server'
 
-export default clerkMiddleware()
+const ProtectedRoute = createRouteMatcher(['/dashboard(.*)'])
+
+export default clerkMiddleware(async (auth , req) => {
+    if(ProtectedRoute(req)){
+        await auth.protect()
+    }
+})
 
 export const config = {
     matcher: [
