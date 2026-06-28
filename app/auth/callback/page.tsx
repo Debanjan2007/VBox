fc63a1509de90c1c2bb54141f008840e0cbad11b
Debanjan Das <microsoft2420@gmail.com>
2026-06-28 20:18:55 +0530

    Add redirect url logic to sign up or sign in

---
+++ app/auth/callback/page.tsx
@@ -0,0 +1,29 @@
+﻿import { currentUser } from "@clerk/nextjs/server"
+import { NextResponse } from "next/server"
+import { redirect } from "next/navigation"
+
+export default async function GET(){
+    try {
+        const user = await currentUser()
+        console.log("user is :",user)
+        if(!user){
+            return NextResponse.json(
+                {
+                    success: false,
+                    user: user,
+                    message: "Got 0 user"
+                }
+            )
+            // redirect("/signup")
+        }
+        redirect("/dashboard")
+    } catch (error) {
+        console.log(error)
+        return NextResponse.json(
+            {
+                success: false,
+                message: "Fatal! something went wrong"
+            }
+        )
+    }
+}
