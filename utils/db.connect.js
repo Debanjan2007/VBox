8022055cff4d4ac7dacd936daf523178cbb3e688
Debanjan Das <microsoft2420@gmail.com>
2026-06-29 11:00:44 +0530

    Add mongodb connection function

---
+++ utils/db.connect.js
@@ -0,0 +1,13 @@
+﻿import mongoose from "mongoose"
+
+const DB_URI = process.env.DATABASE_URI
+const DB_PATH = process.env.DATABASE_PATH
+export const dbConnect = async () => {
+    try {
+        const conn = await mongoose.connect(`${DB_URI}/${DB_PATH}`)
+        console.log(`MongoDB Connected: ${conn.connection.host}`)
+    } catch (error) {
+        console.log(`Error : ${error.message}`);        
+        process.exit(1)
+    }
+}
