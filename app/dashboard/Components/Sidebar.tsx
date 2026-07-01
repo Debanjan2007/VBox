e07078248d82b5c2a1c590d09b45c3d823496cb0
Debanjan Das <microsoft2420@gmail.com>
2026-07-02 00:15:55 +0530

    Add sidebar to the layout

---
+++ app/dashboard/Components/Sidebar.tsx
@@ -0,0 +1,80 @@
+﻿"use client"
+
+import { Home, Settings, Monitor, Camera, LayoutTemplate, Database, WifiPen } from "lucide-react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export const SideBaritems = [
+    {
+        icon: <Home className="w-4 h-4 text-muted-foreground" />,
+        label: "Dashboard",
+        href: "/dashboard"
+    },
+    {
+        icon: <Monitor className="w-4 h-4 text-muted-foreground" />,
+        label: "Machines",
+        href: "/machines"
+    },
+    {
+        icon: <Camera className="w-4 h-4 text-muted-foreground" />,
+        label: "Snapshots",
+        href: "/snapshots"
+    },
+    {
+        icon: <LayoutTemplate className="w-4 h-4 text-muted-foreground" />,
+        label: "Templates",
+        href: "/templates"
+    },
+    {
+        icon: <Database className="w-4 h-4 text-muted-foreground" />,
+        label: "Storage",
+        href: "/storage"
+    },
+    {
+        icon: <WifiPen className="w-4 h-4 text-muted-foreground" />,
+        label: "Networks",
+        href: "/networks"
+    },
+    {
+        icon: <Settings className="w-4 h-4 text-muted-foreground" />,
+        label: "Settings",
+        href: "/settings"
+    },
+]
+
+function SideBarItem({ icon, label, href , page }: { icon: React.ReactNode; label: string; href: string; page: string }) {
+    const pathname = usePathname();
+    return (
+        <Link href={href} className={
+            pathname === page && pathname === href
+                ? "flex w-full h-12 items-center text-muted-foreground/90 flex gap-2 bg-muted/70 p-2 pl-4 rounded-md bg-muted/50 transition-all duration-300"
+                :
+            "flex w-full h-12 items-center text-muted-foreground flex gap-2 p-2 pl-4 rounded-md hover:bg-muted/50 transition-all duration-300"}
+        >
+            {icon}
+            <span className="text-sm">{label}</span>
+        </Link>
+    )
+}
+
+export function Sidebar({page}: {page: string}) {
+    return (
+        <div className="container hidden md:flex flex flex-col w-1/4 h-full gap-2 border-r-1 border-muted-foreground/20">
+            <div className="container hidden md:flex h-full flex-col gap-2 p-4">
+                {SideBaritems.map((item, index) => (
+                    <SideBarItem key={index} icon={item.icon} label={item.label} href={item.href} page={page} />
+                ))}
+            </div>
+            <div className="profile p-4  w-full mb-2">
+                <div className="page flex h-14 w-full gap-2 bg-muted border-1 border-muted-foreground/20 rounded-sm p-2 items-center justify-start">
+                    <div className="avatar h-10 w-10 rounded-full bg-blue-100 flex items-center justify-center">
+                    </div>
+                    <div className="userName flex flex-col">
+                        <h2 className="font-semibold text-muted-foreground">Debanjan</h2>
+                        <p className="text-muted-foreground/70 text-sm">example15@gmail.com</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,10 +1,10 @@
 ﻿import { AnimatedGridPattern } from "@/components/ui/animated-grid-pattern"
 import { MobileMenu } from "./Components/MobileMenu"
 import Image from "next/image";
-import { Bell, Settings, User } from 'lucide-react';
 import Link from "next/link";
 import { SearchBar } from "./Components/Search"
 import { ControllPanel } from "./Components/ControllPanel"
+import { Sidebar } from "./Components/Sidebar"
 
 export default function Layout({
     children
@@ -15,7 +15,7 @@ export default function Layout({
         <div className="container h-screen w-screen flex flex-col">
             <AnimatedGridPattern className="absolute inset-0 opacity-35 text-slate-500/60 dark:text-slate-400/40" />
             <nav>
-                <div className="container w-full h-14 flex justify-between items-center px-4">
+                <div className="container w-full h-14 flex justify-between items-center px-4 border-b-1 border-muted-foreground/20">
                     <div className="logo-sec flex items-center gap-2 hidden md:flex">
                         <Image src="/logo.png" alt="logo" height={36} width={36}/>
                         <h1 className="text-lg font-semibold">VBox</h1>
@@ -31,7 +31,8 @@ export default function Layout({
                     </div>
                 </div>
             </nav>
-            <div className="children w-full h-full">
+            <div className="children w-full h-full flex">
+                <Sidebar page="/dashboard" />
                 {children}
             </div>
         </div>
